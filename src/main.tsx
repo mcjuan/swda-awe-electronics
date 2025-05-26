b8a7adac9e7624a2858b2fc66e5a8963622d11c6
Marcus <51019650+mcjuan@users.noreply.github.com>
2025-05-26 16:26:31 +1000

    cart context
    cart provider, cart item type

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.tsx";
+import { CartProvider } from "./context/CartContext"; // Import CartProvider
+
 import "./index.css";
 import App from "./App.tsx";
 
@@ -9,7 +11,9 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <CartProvider>
+          <App />
+        </CartProvider>
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>
